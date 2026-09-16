568bd4621230c97617fd438327d3319fe4eec32e
aXXo-dev <0381129@csvdc.qc.ca>
2026-09-16 13:17:31 -0400

    feat: Placeholder

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-    <span>Coming soon...</span>
+    <main className="w-screen h-dvh flex justify-center items-center bg-linear-to-b from-[#415AFF] to-[#4758C7]">
+      <span className="text-3xl md:text-6xl font-black text-center italic text-black text-shadow">Coming soon...</span>
+    </main>
   );
 }
