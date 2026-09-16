0a6003eedc12a6360fe9e91eb77ed157cc49d7ad
aXXo-dev <0381129@csvdc.qc.ca>
2026-09-16 13:12:51 -0400

    chore: Config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  pageExtensions: ["js", "jsx", "ts", "tsx"],
+  experimental: {
+    optimizePackageImports: ["lucide-react"],
+  },
+  images: {
+    unoptimized: true,
+    minimumCacheTTL: 604800,
+    formats: ["image/avif", "image/webp"],
+  },
 };
 
 export default nextConfig;
